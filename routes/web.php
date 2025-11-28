be2cebcb558a7563ca3d09649e6bd6f7379738f1
Sayid Adam <mryunkaka@gmail.com>
2025-11-28 18:04:51 +0800

    Add public welcome page for SIPEG

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
+
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
