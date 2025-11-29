07b0335368f42a1b9bed209cae3bb0021328994f
Sayid Adam <mryunkaka@gmail.com>
2025-11-29 09:47:08 +0800

    Masih Fix Select Login

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -11,49 +11,45 @@ class UnitSeeder extends Seeder
     {
         $now = now();
 
-        DB::table('units')->truncate();
-
-        DB::table('units')->insert([
+        $units = [
             [
                 'nama_unit'   => 'HOTEL HARMONY',
                 'alamat_unit' => 'Jl. Raya Batulicin, ...',
                 'no_hp_unit'  => '087878987654',
-                'logo_unit'   => null,
-                'created_at'  => $now,
-                'updated_at'  => $now,
             ],
             [
                 'nama_unit'   => 'GUESTHOUSE RUMA',
                 'alamat_unit' => 'Jl. Suryagandamana, ...',
                 'no_hp_unit'  => '087877521992',
-                'logo_unit'   => null,
-                'created_at'  => $now,
-                'updated_at'  => $now,
             ],
             [
                 'nama_unit'   => 'HOTEL GALERY',
                 'alamat_unit' => 'Jl. Pangeran Hidayat No.26, ...',
                 'no_hp_unit'  => '085827191234',
-                'logo_unit'   => null,
-                'created_at'  => $now,
-                'updated_at'  => $now,
             ],
             [
                 'nama_unit'   => 'HOTEL KARTIKA',
                 'alamat_unit' => 'Jl. Veteran No.2, ...',
                 'no_hp_unit'  => '082150942567',
-                'logo_unit'   => null,
-                'created_at'  => $now,
-                'updated_at'  => $now,
             ],
             [
                 'nama_unit'   => 'HOTEL LAVENDER',
                 'alamat_unit' => 'Jl. Raya provinsi No. km 163, ...',
                 'no_hp_unit'  => '085289987654',
-                'logo_unit'   => null,
-                'created_at'  => $now,
-                'updated_at'  => $now,
             ],
-        ]);
+        ];
+
+        foreach ($units as $unit) {
+            DB::table('units')->updateOrInsert(
+                ['nama_unit' => $unit['nama_unit']], // kunci unik
+                [
+                    'alamat_unit' => $unit['alamat_unit'],
+                    'no_hp_unit'  => $unit['no_hp_unit'],
+                    'logo_unit'   => null,
+                    'created_at'  => $now,
+                    'updated_at'  => $now,
+                ]
+            );
+        }
     }
 }
